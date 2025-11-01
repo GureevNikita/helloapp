4b7aa16e1ec276103ec1c3aea3ddba0d166bb42a
black <black@example.com>
2025-11-01 23:50:48 +0500

    приветствие с со всеми по очереди

--- HelloProgram/Program.cs
+++ HelloProgram/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("hello everyone");
+            greetBlack();
+            greetWhite();
         }
 
         static void greetWhite()
