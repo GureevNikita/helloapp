12ff8226c9ca62f604d408f7fa26ce256ba1ad3e
white <white@example.com>
2025-11-01 23:19:08 +0500

    make greetengs

--- HelloProgram/Program.cs
+++ HelloProgram/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace HelloProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello!");
+        }
+
+        static void greetWhite()
+        {
+            Console.WriteLine("hello white!");
+        }
+    }
+}
